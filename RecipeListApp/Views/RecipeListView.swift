2dd4cab6adb78284200e9c95261fc57c9e3dd245
Confidence Peters <confidence.peter.7@gmail.com>
2021-09-29 05:42:15 +0100

    - Add LazyVStack and ScrollView on the recipe list in RecipeListView.swift - Remove naviagtionBarTitle and replace with Text - Hide navigation bar

--- RecipeListApp/Views/RecipeListView.swift
+++ RecipeListApp/Views/RecipeListView.swift
@@ -15,24 +15,38 @@ struct RecipeListView: View {
     var body: some View {
         
         NavigationView {
-            List(model.recipes) { r in
-                
-               NavigationLink(
-                destination: RecipeDetailView(recipe: r),
-                label: {
-                    HStack(spacing: 15.0){
-                        Image(r.image)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .scaledToFill()
-                            .frame(width: 50, height: 50, alignment: .center)
-                            .clipped()
-                        Text(r.name)
+            
+            VStack(alignment: .leading) {
+                Text("All Recipes")
+                    .bold()
+                    .padding(.top, 30)
+                    .font(.title)
+                    
+                ScrollView {
+                    LazyVStack(alignment: .leading) {
+                        ForEach(model.recipes) { r in
+                            
+                           NavigationLink(
+                            destination: RecipeDetailView(recipe: r),
+                            label: {
+                                HStack(spacing: 15.0){
+                                    Image(r.image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .scaledToFill()
+                                        .frame(width: 50, height: 50, alignment: .center)
+                                        .clipped()
+                                    Text(r.name)
+                                        .foregroundColor(.black)
+                                }
+                            })
+                           
+                        }
+                        .navigationBarHidden(true)
+                        
                     }
-                })
-               
-            }
-            .navigationBarTitle("All Recipes")
+                }
+            }.padding(.leading)
         }
     }
 }
@@ -40,5 +54,6 @@ struct RecipeListView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView().previewDevice("iPhone 12")
+            .environmentObject(RecipeViewModel())
     }
 }
